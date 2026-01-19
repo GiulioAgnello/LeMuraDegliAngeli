843fa0cef824cdea662536267b3bea17220c7120
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2026-01-19 12:44:38 +0100

    addMap-React

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,20 @@
+import React from "react";
+import GoogleMapReact from "google-map-react";
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
 export default function Footer() {
+  const defaultProps = {
+    center: {
+      lat: 40.22099765864096,
+      lng: 18.227172181072095,
+    },
+    zoom: 11,
+  };
+
   return (
     <>
       <div className="container">
         <h2 className="text-center mb-5">Dove Siamo</h2>
+
         <div className="row">
           <div className="col-12 col-md-6 mb-4">
             <h5>Strategia nel Salento</h5>
@@ -15,11 +27,19 @@ export default function Footer() {
             </ul>
           </div>
           <div className="col-12 col-md-6">
-            <img
-              src="/images/mappa.jpg"
-              alt="Mappa Salento"
-              className="img-fluid rounded"
-            />
+            <div style={{ height: "20vh", width: "100%" }}>
+              <GoogleMapReact
+                bootstrapURLKeys={{ key: "" }}
+                defaultCenter={defaultProps.center}
+                defaultZoom={defaultProps.zoom}
+              >
+                <AnyReactComponent
+                  lat={59.955413}
+                  lng={30.337844}
+                  text="My Marker"
+                />
+              </GoogleMapReact>
+            </div>
           </div>
         </div>
       </div>
