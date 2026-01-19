89a6fca59612829e50c7303dbffaf7a2b317e52a
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2026-01-19 12:24:18 +0100

    footerCreate

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,28 @@
 export default function Footer() {
   return (
     <>
-      <h2>Footer</h2>
+      <div className="container">
+        <h2 className="text-center mb-5">Dove Siamo</h2>
+        <div className="row">
+          <div className="col-12 col-md-6 mb-4">
+            <h5>Strategia nel Salento</h5>
+            <p>Situati nel cuore del Salento, siamo a pochi km da:</p>
+            <ul>
+              <li>Spiagge di Lecce e Porto Cesareo</li>
+              <li>Centro storico di Lecce (Barocco leccese)</li>
+              <li>Melendugno e le cale salentine</li>
+              <li>Torre dell'Orso</li>
+            </ul>
+          </div>
+          <div className="col-12 col-md-6">
+            <img
+              src="/images/mappa.jpg"
+              alt="Mappa Salento"
+              className="img-fluid rounded"
+            />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -153,30 +153,7 @@ export default function Homepage() {
       </section>
 
       {/* Posizione */}
-      <section className="py-5">
-        <div className="container">
-          <h2 className="text-center mb-5">Dove Siamo</h2>
-          <div className="row">
-            <div className="col-12 col-md-6 mb-4">
-              <h5>Strategia nel Salento</h5>
-              <p>Situati nel cuore del Salento, siamo a pochi km da:</p>
-              <ul>
-                <li>Spiagge di Lecce e Porto Cesareo</li>
-                <li>Centro storico di Lecce (Barocco leccese)</li>
-                <li>Melendugno e le cale salentine</li>
-                <li>Torre dell'Orso</li>
-              </ul>
-            </div>
-            <div className="col-12 col-md-6">
-              <img
-                src="/images/mappa.jpg"
-                alt="Mappa Salento"
-                className="img-fluid rounded"
-              />
-            </div>
-          </div>
-        </div>
-      </section>
+      <section className="py-5"></section>
     </>
   );
 }
